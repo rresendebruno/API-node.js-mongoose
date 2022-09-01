769d21106bb5df819adaca80a550ffff70593ff9
Bruno Resende <rresendebruno@gmail.com>
2022-09-01 17:58:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// config inicial
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// forma de ler JSON / middlewares
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// rotas da API
+const personRoutes = require("./routes/personRoutes");
+
+app.use("/person", personRoutes);
+
+// rota inicial / endpoint
+app.get("/", (req, res) => {
+  // mostrar req
+  res.json({ message: "Oi Express!" });
+});
+
+// entregar uma porta / conexão com banco de dados mongoDB
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.ufysmvs.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Conectamos ao MongoDB");
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
